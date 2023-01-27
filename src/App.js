1facb56bdf8f837220a8523e25b7e7524abefca6
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-01-28 03:32:26 +0800

    stable with navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = "app">
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="stylesheet" href="App.css" />
+      <title>Ez Mil</title>
+      <header>
+      <Navbar />
       </header>
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta iusto
+          autem quae provident sed ipsam laudantium quos, eveniet fugit beatae
+          similique vitae repellat, fugiat animi exercitationem accusamus, totam
+          quaerat? Nam, fugiat? Perferendis adipisci deleniti accusantium, illum
+          quis harum. Magni animi placeat vero deleniti molestias impedit,
+          aspernatur minus. Ea minima, quisquam, facilis assumenda consequatur
+          dicta velit eaque optio, consectetur neque cum unde tenetur numquam.
+          Hic.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ratione
+          debitis consectetur, cupiditate tempore quae sed dolor dolorum
+          nesciunt animi. Autem fuga maiores cum quia, minus aliquid odit
+          repellendus molestiae. Excepturi perferendis sequi obcaecati
+          recusandae ratione optio ex sint ducimus corrupti neque impedit,
+          architecto minus!
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          voluptates modi ducimus neque omnis itaque recusandae impedit cum
+          aliquid maxime? Nam vero quis et omnis quos necessitatibus repellat
+          deserunt facilis! Aliquam temporibus dolorum sint cumque ipsam placeat
+          non quisquam ad facere quod commodi magni, laboriosam repudiandae
+          tempora provident distinctio? Velit asperiores amet dolorum esse.
+          Fugiat, enim recusandae modi id saepe eligendi maiores libero sint
+          nulla ut quisquam numquam autem voluptates labore dolore at.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ratione
+          debitis consectetur, cupiditate tempore quae sed dolor dolorum
+          nesciunt animi. Autem fuga maiores cum quia, minus aliquid odit
+          repellendus molestiae. Excepturi perferendis sequi obcaecati
+          recusandae ratione optio ex sint ducimus corrupti neque impedit,
+          architecto minus!
+        </p>
+      </main>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import logo from "../../img/logo.svg"
+import logocombo from "../../img/logocombnobg.svg"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./Navbar.css";
+
+
+library.add(faFacebook, faInstagram, faTwitter, faYoutube, faSearch, faBars)
+
+
+
+function Navbar() {
+  const [navbarActive, setNavbarActive] = useState(false);
+
+  useEffect(() => {
+
+    const menuBtn = document.getElementById('menu-btn');
+    const menu = document.querySelector('.menu');
+    menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('menu-open')
+    });
+
+    const handleScroll = () => {
+      const offset = 50;
+      if (window.scrollY > offset) {
+        setNavbarActive(true);
+      } else {
+        setNavbarActive(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  
+    return (
+        <nav id="navbar" className={navbarActive ? 'navbar-active' : ''}>
+        <div className="menu">
+          <div>
+            <img src={logo} className="logo" alt="" />
+            <ul>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">What's New</a>
+              </li>
+              <li>
+                <a href="#">Categories</a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href="#">Projects</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+              <li>
+                <a href="#">Store</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+            <ul className="social-medias">
+              <a href="#">
+                <FontAwesomeIcon icon={['fab', 'facebook']} />
+              </a>
+              <a href="#">
+                <FontAwesomeIcon icon={['fab', 'instagram']} />
+              </a>
+              <a href="#">
+                <FontAwesomeIcon icon={['fab', 'twitter']} />
+              </a>
+              <a href="#">
+                <FontAwesomeIcon icon={['fab', 'youtube']} />
+              </a>
+            </ul>
+            <form>
+              <div className="input-wrap">
+                <input type="search" placeholder="Search..." />
+                <button type="submit">
+                  <i className="fas fa-search" />
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="container">
+          <img className="brand" src={logocombo} alt="" />
+          <div className="container-inner">
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Discography</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+            <form>
+              <div className="input-wrap">
+                <input placeholder="Search..." type="search" />
+                <button type="submit">
+                  <FontAwesomeIcon icon="search" />
+                </button>
+              </div>
+            </form>
+          </div>
+          <i className="fas fa-bars" id="menu-btn"><FontAwesomeIcon icon={faBars} /></i>
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar;
