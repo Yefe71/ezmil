12d4b3e0b5a5bd26c17a9b9d938acaee5569680a
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-02-05 18:33:45 +0800

    mint done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -25,6 +25,9 @@ import shorts from "../../container/img/Photos/Merch/shorts.webp"
 import tank from "../../container/img/Photos/Merch/tank.webp"
 import towel from "../../container/img/Photos/Merch/towel.webp"
 import tshirt from "../../container/img/Photos/Merch/tshirt.webp"
+import nftdud from "../../container/img/Photos/nft/nft.png"
+import illalogo from "../../container/img/Photos/nft/illa.png"
+import qr from "../../container/img/Photos/nft/qr.png"
 // import required modules
 import { Pagination, Navigation, Autoplay } from "swiper";
 import { hex3 } from "@react-spring/shared";
@@ -42,10 +45,14 @@ export default function SwiperComp(props) {
     <div id="swiper-comp">
       <Swiper
         autoplay={{ delay: 10000 }}
-        modules={[Pagination, Autoplay]}
+        modules={[Pagination, Autoplay, Navigation]}
 
         className={HeroCSS.swiper}
         loop={true}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
         pagination={{
           dynamicBullets: true,
         }}
@@ -66,45 +73,100 @@ export default function SwiperComp(props) {
             <h2 className={HeroCSS.bottom}>OUT NOW</h2>
           </div>
         </SwiperSlide>
+        
         <SwiperSlide
           className={HeroCSS.swiperSlide}
           style={{ backgroundImage: `url(${ezbg})` }}
         >
-          <div className= {HeroCSS.merchparent}>
+   
 
-            <div className={HeroCSS.left2}>
-              <div className={HeroCSS.item}>
-                <img className={HeroCSS.pic} src={tshirt} alt="" />
+          <div className= {HeroCSS.merchparent}>
+          <div className={HeroCSS.btncontainer}>
+              <a  href = "https://shop.virginmusic.com/collections/ez-mil" rel="noreferrer" target="_blank" className={HeroCSS.buy}>           
+                  BUY NOW
+                </a>
+            </div>
+
+            <div className={HeroCSS.right2}>
+            <div className={`${HeroCSS.item} ${HeroCSS.five}`}>
+                <img className={HeroCSS.pic} src={blackhoodie} alt="" />
+                <h1 className={HeroCSS.fivetitle}>Ez Mil - Black Hoodie</h1>
               </div>
-              <div className={HeroCSS.item}>
-                <img className={HeroCSS.pic} src={tank} alt="" />
+
+              <div className={`${HeroCSS.item} ${HeroCSS.six}`}>
+                <img className={HeroCSS.pic} src={redhoodie} alt="" />
+                <h1 className={HeroCSS.sixtitle}>Ez Mil - Red Hoodie</h1>
               </div>
-              <div className={HeroCSS.item}>
-                <img className={HeroCSS.pic} src={longsleeve} alt="" />
+
+              <div className={`${HeroCSS.item} ${HeroCSS.seven}`}>
+                <img className={HeroCSS.pic} src={towel} alt="" />
+                <h1 className={HeroCSS.seventitle}>Ez Mil - Towel</h1>
               </div>
-              <div className={HeroCSS.item}>
-                <img className={HeroCSS.pic} src={shorts} alt="" />
+
+              <div className={`${HeroCSS.item} ${HeroCSS.eight}`}>
+                <img className={HeroCSS.pic} src={pillow} alt="" />
+                <h1 className={HeroCSS.eighttitle}>Ez Mil - Pillow</h1>
               </div>
+
             </div>
 
             <div className={HeroCSS.mid2}>
               <img className={HeroCSS.merchpic} src={merchpic} alt="" />
             </div>
 
-            <div className={HeroCSS.right2}>
-            <div className={HeroCSS.item}>
-                <img className={HeroCSS.pic} src={blackhoodie} alt="" />
+            <div className={HeroCSS.left2}>
+              <div className={`${HeroCSS.item} ${HeroCSS.one}`}>
+                <img className={`${HeroCSS.pic}`} src={tshirt} alt="" />
+                <h1 className={HeroCSS.onetitle}>Ez Mil - T-Shirt</h1>
               </div>
-              <div className={HeroCSS.item}>
-                <img className={HeroCSS.pic} src={redhoodie} alt="" />
+              <div className={`${HeroCSS.item} ${HeroCSS.two}`}>
+                <img className={HeroCSS.pic} src={tank} alt="" />
+                <h1 className={HeroCSS.twotitle}>Ez Mil - Tank</h1>
               </div>
-              <div className={HeroCSS.item}>
-                <img className={HeroCSS.pic} src={towel} alt="" />
+              <div className={`${HeroCSS.item} ${HeroCSS.three}`}>
+                <img className={HeroCSS.pic} src={longsleeve} alt="" />
+                <h1 className={HeroCSS.threetitle}>Ez Mil - Longsleeve</h1>
               </div>
-              <div className={HeroCSS.item}>
-                <img className={HeroCSS.pic} src={pillow} alt="" />
+              <div className={`${HeroCSS.item} ${HeroCSS.four}`}>
+                <img className={HeroCSS.pic} src={shorts} alt="" />
+                <h1 className={HeroCSS.fourtitle}>Ez Mil - All Over Print Shorts</h1>
+
               </div>
             </div>
+
+
+
+
+          </div>
+          
+        </SwiperSlide>
+
+        <SwiperSlide
+          className={HeroCSS.swiperSlide}
+          style={{ background: 'rgb(227, 204, 173)' }}
+        >
+
+          <div className= {HeroCSS.illaparent}>
+
+          <div className={HeroCSS.btncontainermint}>
+            <a  href = "https://app.niftykit.com/drops/illazillazelementz" rel="noreferrer" target="_blank" className={HeroCSS.mint}>           
+                MINT NOW
+              </a>
+          </div>
+
+          <div className={HeroCSS.left3}>
+          <img src={illalogo} alt="" className={HeroCSS.illalogo} />
+          <div className={HeroCSS.pwrapper}>          
+          <p>Slashzilla holders will receive a copy of an original story board booklet from the music video of his upcoming single "Rapture". Stay tuned for more announcements.</p>
+</div>
+          </div>
+
+
+       
+
+          <div className={HeroCSS.right3}>
+            <img src={nftdud} alt="" className={HeroCSS.nftdud} />
+          </div>
 
           </div>
           
@@ -112,6 +174,8 @@ export default function SwiperComp(props) {
 
 
       </Swiper>
+      <div className={HeroCSS.swiperbuttonprev}></div>
+      <div className={HeroCSS.swiperbuttonnext}></div>
     </div>
   );
 }
