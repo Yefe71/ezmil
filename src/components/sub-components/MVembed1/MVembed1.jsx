4c751ac43026fd49d12bc9bc43f43aaecf0f8d67
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-02-27 00:27:59 +0800

    mv 98%

--- src/components/sub-components/MVembed1/MVembed1.jsx
+++ src/components/sub-components/MVembed1/MVembed1.jsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./MVembed1.module.css";
 
 export const MVembed1 = (props) => {
+  const thumbRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
+
+  const handleThumbClick = () => {
+    setIsActive(true);
+  };
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (thumbRef.current && !thumbRef.current.contains(e.target)) {
+        setIsActive(false);
+      }
+    };
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
 
   return (
-    <div
-      className={styles.thumbContainer}
-      style={{
-        backgroundImage: `url(${props.thumb})`,
-        ...(props.link === "abKwhST8Qv0" && { backgroundPosition: "top" }),
-      }}
-    >
-<div className={styles.titleWrapper}>
-
-
-      <h3 className = {styles.title}>{props.title}</h3>
-</div>
-
+    <div className={styles.wrapper}>
+      <div className={styles.titleWrapper} ref={thumbRef} onClick={handleThumbClick}>
+        <h3 className={styles.title}>{props.title}</h3>
+      </div>
+      <div
+        
+        className={`${styles.thumbContainer} ${isActive ? styles.active : ""}`}
+        style={{
+          backgroundImage: `url(${props.thumb})`,
+          ...(props.link === "abKwhST8Qv0" && { backgroundPosition: "top" }),
+        }}
+        
+      ></div>
     </div>
   );
 };
