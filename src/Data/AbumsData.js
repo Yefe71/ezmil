a72576569f29b63f039c29bb4724f3f8b6cfbbac
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-02-28 23:36:09 +0800

    fixed cube captions

--- src/Data/AbumsData.js
+++ src/Data/AbumsData.js
@@ -4,10 +4,11 @@ const AlbumsData = [
     date: "July 27, 2022",
     id: "4kAZzYkAxND103ygB4tZ3a",
     thumb: "thumbs/duality.jpg",
-    desc: `Delve into the ethereal universe of Ez Mil's "DU4LI7Y," a 10-song album by EZ Mil that takes listeners on a journey through Mareipas, Earth, and Proxima B. All tracks play on an ethereal universe, with a lore that involves a cast of characters, including Ez, Slashmouth, Prince Milreth, R4C7, The Queen, Princess/Captain Raylin, and The Emprezz. Each title has individual stories, and the album is meant to be listened to back-to-back, with everything connected from song number 1 to song number 10.`,
+    desc: `Delve into the ethereal universe of Ez Mil's "DU4LI7Y," a 10-track album by EZ Mil that takes listeners on a journey through Mareipas, Earth, and Proxima B. All tracks play on an ethereal universe, with a lore that involves a cast of characters, including Ez, Slashmouth, Prince Milreth, R4C7, The Queen, Princess/Captain Raylin, and The Emprezz. Each title has individual stories, and the album is meant to be listened to back-to-back, with everything connected from song number 1 to song number 10.`,
     length: "00:35:34",
     spotLink: "https://open.spotify.com/album/4kAZzYkAxND103ygB4tZ3a",
-    appleLink: "https://music.apple.com/us/album/du4li7y/1634313076"
+    appleLink: "https://music.apple.com/us/album/du4li7y/1634313076",
+    youLink: "https://www.youtube.com/watch?v=zBrbpKTycxo&list=OLAK5uy_l0l04yvdjpayOH1lkdg5d2NgptB-83pjs"
   
   },
   {
@@ -18,7 +19,8 @@ const AlbumsData = [
     desc: `Feel enamored with "Resonances", a 7-track EP that predominantly features acoustic guitar, piano, and drums, creating a soothing and relaxed vibe that stands in contrast to the dynamic and lively energy of Act 1. The EP explores the different aspects of love. From heartbreak to obsession, "Resonances" takes you on an emotional journey. This EP marks the first of many to come in between albums, showcasing the artist's emotional range and musical prowess.`,
     length: "00:18:29",
     spotLink: "https://open.spotify.com/album/4R6HwGLuuLuXeSxitNtDnJ",
-    appleLink: "https://music.apple.com/us/album/resonances/1566972776"
+    appleLink: "https://music.apple.com/us/album/resonances/1566972776",
+    youLink: "https://www.youtube.com/watch?v=oIFlt0Ha_1I&list=OLAK5uy_mES81lC6j9XK6YHaJKHTwq-V0tWM6lcb4"
   },
   {
     title: "Act 1",
@@ -28,7 +30,8 @@ const AlbumsData = [
     desc: `Explore Ez Mil's debut album "Act 1", which consists of 16 tracks. with the first eight songs being clean, melodic pop tracks. However, the album's second half takes a dramatic turn, with a focus on hardcore rap and more serious themes. Throughout the album, the artist explores various experiences and emotions, with some tracks being playful and lighthearted, while others delve into deeper, darker territory. By the end of the album, the artist is ready to take on whatever comes their way, having expressed their feelings and experiences through their music.`,
     length: "01:00:07",
     spotLink: "https://open.spotify.com/album/7dL6eZG0rMjmXUOwzUfarY",
-    appleLink: "https://music.apple.com/us/album/act-1/1582733812"
+    appleLink: "https://music.apple.com/us/album/act-1/1582733812",
+    youLink: "https://www.youtube.com/watch?v=kLwO2ueepZs&list=OLAK5uy_n-eeQ5uVj-FqTzNIApPTnLkTpK7o5PYUs"
   },
 
 ]
--- src/components/sub-components/Cube/Cube.jsx
+++ src/components/sub-components/Cube/Cube.jsx
@@ -8,6 +8,7 @@ import youtube from "../../../container/img/swiper-logos/youtube.svg";
 import apple from "../../../container/img/swiper-logos/apple.png";
 import arrow from "../../../container/img/arrow.png";
 import arrow2 from "../../../container/img/arrow2.png";
+import AlbumsData from "../../../Data/AbumsData";
 // Import Swiper styles
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -48,72 +49,36 @@ export default function Cube() {
         modules={[EffectCube, Pagination, Autoplay]}
         className={cubeCSS.swiperCubeMain}
       >
-        <SwiperSlide className={cubeCSS.swiperCube}>
-          <img alt="Duality album cover" src={duality}/>
 
-          <div className={cubeCSS.contentWrapper}>
+{AlbumsData.map((albumItem, index) => {
+          return (
+            <SwiperSlide className={cubeCSS.swiperCube}>
+              <img alt="Duality album cover" src={albumItem.thumb} />
+              <div className={cubeCSS.contentWrapper}>
+                <div className={cubeCSS.overlayInfo1}>
+                  <h2 className={cubeCSS.title}>{albumItem.title}</h2>
+                  <p className={cubeCSS.date}>{albumItem.date}</p>
+                  <div className={cubeCSS.descWrapper}>
+                    <p className={cubeCSS.caption}>{albumItem.desc}</p>
+                  </div>
+                </div>
+                <div className={cubeCSS.streamIcons}>
+                  <a href={albumItem.appleLink}>
+                    <img src={apple} className={cubeCSS.applelogo} alt="apple" />
+                  </a>
+                  <a href={albumItem.youLink}>
+                    <img src={youtube} className={cubeCSS.youtubelogo} alt="youtube" />
+                  </a>
+                  <a href={albumItem.spotLink}>
+                    <img src={spotify} className={cubeCSS.spotifylogo} alt="spotify" />
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
 
 
-          <div className={cubeCSS.overlayInfo1}>
-            <h2 className = {cubeCSS.title}>DU4LI7Y</h2>
-            <p className = {cubeCSS.date}>11/07/02</p>
-            <p className = {cubeCSS.caption}>DU4LI7Y explains how he is as a person and his music journey which hasn't been the easiest for him. The reason his album title spelled duality with 4 & 7 was,</p>
-          </div>
-
-          <div className={cubeCSS.streamIcons}>
-
-            <a href="https://music.apple.com/us/album/du4li7y/1634313076"><img src={apple} className = {cubeCSS.applelogo} alt="apple" /></a>
-            <a href="https://www.youtube.com/watch?v=zBrbpKTycxo&list=OLAK5uy_l0l04yvdjpayOH1lkdg5d2NgptB-83pjs"><img src={youtube} className = {cubeCSS.youtubelogo} alt="youtube" /></a>
-            <a href="https://open.spotify.com/album/4kAZzYkAxND103ygB4tZ3a"><img src={spotify} className = {cubeCSS.spotifylogo}alt="spotify" /></a>
-
-          </div>
-
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className={cubeCSS.swiperCube}>
-          <img src={act1} alt="Act 1 album cover"/>
-
-        <div className={cubeCSS.contentWrapper}>
-        <div className={cubeCSS.overlayInfo1}>
-            <h2 className = {cubeCSS.title}>ACT 1</h2>
-            <p className = {cubeCSS.date}>11/07/02</p>
-            <p className = {cubeCSS.caption}>DU4LI7Y explains how he is as a person and his music journey which hasn't been the easiest for him. The reason his album title spelled duality with 4 & 7 was,</p>
-          </div>
-
-          <div className={cubeCSS.streamIcons}>
-
-            <a href="https://music.apple.com/us/album/act-1/1582733812"><img src={apple} alt="apple" /></a>
-            <a href="https://www.youtube.com/watch?v=kLwO2ueepZs&list=OLAK5uy_n-eeQ5uVj-FqTzNIApPTnLkTpK7o5PYUs"><img src={youtube} className = {cubeCSS.youtubelogo} alt="youtube" /></a>
-            <a href="https://open.spotify.com/album/7dL6eZG0rMjmXUOwzUfarY"><img src={spotify} alt="spotify" /></a>
-
-          </div>
-
-        </div>
-
-        </SwiperSlide>
-        <SwiperSlide className={cubeCSS.swiperCube}>
-          <img src={resonances} alt="Resonances album cover"/>
-
-          <div className={cubeCSS.contentWrapper}>
-
-          <div className={cubeCSS.overlayInfo1}>
-              <h2 className = {cubeCSS.title}>RESONANCES</h2>
-              <p className = {cubeCSS.date}>11/07/02</p>
-              <p className = {cubeCSS.caption}>DU4LI7Y explains how he is as a person and his music journey which hasn't been the easiest for him. The reason his album title spelled duality with 4 & 7 was,</p>
-            </div>
-
-            <div className={cubeCSS.streamIcons}>
-
-              <a href="https://music.apple.com/us/album/resonances/1566972776"><img src={apple} alt="apple" /></a>
-              <a href="https://www.youtube.com/watch?v=oIFlt0Ha_1I&list=OLAK5uy_mES81lC6j9XK6YHaJKHTwq-V0tWM6lcb4"><img src={youtube} className = {cubeCSS.youtubelogo} alt="youtube" /></a>
-              <a href="https://open.spotify.com/album/3JUIV1peZnOQlNF0cqRPvZ"><img src={spotify} alt="spotify" /></a>
-
-            </div>
-          </div>
-
-            
-    
-        </SwiperSlide>
         {/* <div className={cubeCSS.swiperbuttonprev3}>
           <img src={arrow}  onClick={handlePrev} alt="" />
         </div>
