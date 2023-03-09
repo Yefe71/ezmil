b0543a2cf4d9bc281b1689c8c74dd68cfa74944d
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-03-10 04:25:10 +0800

    fixed most of  home, change news layout

--- src/components/HomeNav/HomeNav.jsx
+++ src/components/HomeNav/HomeNav.jsx
@@ -20,7 +20,7 @@ const HomeNav = ({ onChildData }) => {
     <div className = {homecss.homeNavParent} style={{ backgroundImage: `url(${bgnav})` }}>
         <div className={homecss.navWrapper}>
 
-          <h3 className={`${homecss.newStuff} ${homecss.navLink} ${activeLink === 'link1' ? homecss.active:''}`} onClick = {() => handleLinkClick('component1','link1')}>NEW STUFF</h3>
+          <h3 className={`${homecss.newStuff} ${homecss.navLink} ${activeLink === 'link1' ? homecss.active:''}`} onClick = {() => handleLinkClick('component1','link1')}>RELEASES</h3>
           <h3 className={`${homecss.musicVideos} ${homecss.navLink} ${activeLink === 'link2' ? homecss.active:''}`} onClick = {() => handleLinkClick('component2','link2')}>MUSIC VIDEOS</h3>
           <h3 className={`${homecss.news} ${homecss.navLink} ${activeLink === 'link3' ? homecss.active:''}`} onClick = {() => handleLinkClick('component3','link3')}>NEWS</h3>
 
